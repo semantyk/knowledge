e9f5db140095180408d371f7185161676ec166a2
Daniel B. <danielbakas@gmail.com>
2024-12-02 00:21:30 -0600

    Update regex.py

--- util/fragments/regex.py
+++ util/fragments/regex.py
@@ -22,7 +22,7 @@ import uuid
 
 def main():
     f = open("fragments.txt", "a")
-    for _ in range(1000000):
+    for _ in range(10000):
         identifier = uuid.uuid4().hex
         identifier = random.choice(string.ascii_letters[0:6]) + identifier[1:8] + '_' + identifier[8:12] + '_' + \
             identifier[12:16] + '_' + identifier[16:20] + \
