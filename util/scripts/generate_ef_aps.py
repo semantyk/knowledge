ba3e5a63d6bf50eec2cc28ce6a1a4f428301e8d6
Daniel B. <danielbakas@gmail.com>
2024-12-15 16:17:15 -0600

    Add Powers to each EF
    • Add Executive Power
    • Add Legislative Power
    • Add Judicial Power

--- util/scripts/generate_ef_aps.py
+++ util/scripts/generate_ef_aps.py
@@ -41,9 +41,9 @@ class RDFProcessor:
 
 
 class EntityCodeGenerator:
-    def __init__(self, graph, entity_class_uri):
+    def __init__(self, graph, power_class_uri):
         self.graph = graph
-        self.entity_class_uri = entity_class_uri
+        self.power_class_uri = power_class_uri
 
         # Bind namespaces for clean prefixes
         self.graph.namespace_manager.bind("rdf", RDF)
@@ -57,85 +57,96 @@ class EntityCodeGenerator:
         identifier = uuid.uuid4().hex
         return f"{random.choice(string.ascii_letters[0:6])}{identifier[1:8]}_{identifier[8:12]}_{identifier[12:16]}_{identifier[16:20]}_{identifier[20:]}"
 
-    def generate_instance(self, entity_name, entity_code):
-        """Generate a federal entity RDF instance."""
-        new_fragment = self._generate_uuid()
-        uri = URIRef(MEX[new_fragment])
+    def generate_instance(self, entity_uri, entity_name):
+        """Generate 3 instances of Power for each federal entity."""
+        power_types_es = ["Ejecutivo", "Legislativo", "Judicial"]
+        power_types_en = ["Executive", "Legislative", "Judicial"]
+        for i in range(3):
+            power_fragment = self._generate_uuid()
+            power_uri = URIRef(MEX[power_fragment])
 
-        self.graph.add((
-            uri,
-            RDF.type,
-            MEX[self.entity_class_uri]
-        ))
-        self.graph.add((
-            uri,
-            SKOS.prefLabel,
-            Literal(entity_name, lang="es")
-        ))
-        self.graph.add((
-            uri,
-            SKOS.prefLabel,
-            Literal(entity_name, lang="en")
-        ))
-        self.graph.add((
-            uri,
-            DCTERMS.isPartOf,
-            URIRef(MEX["cat/cloc#"])
-        ))
-        self.graph.add((
-            uri,
-            RDFS.label,
-            Literal(f"El Código de la Entidad Federal de {entity_name}", lang="es")
-        ))
-        self.graph.add((
-            uri,
-            RDFS.label,
-            Literal(f"The {entity_name} Federal Entity Code", lang="en")
-        ))
-        self.graph.add((
-            uri,
-            RDF.value,
-            Literal(entity_code, datatype=XSD.integer)
-        ))
-        self.graph.add((
-            uri,
-            RDFS.isDefinedBy,
-            URIRef(MEX["fed/cloc/"])
-        ))
+            self.graph.add((
+                power_uri,
+                RDF.type,
+                MEX[self.power_class_uri]
+            ))
+            self.graph.add((
+                power_uri,
+                SKOS.prefLabel,
+                Literal(entity_name, lang="es")
+            ))
+            self.graph.add((
+                power_uri,
+                SKOS.prefLabel,
+                Literal(entity_name, lang="en")
+            ))
+            self.graph.add((
+                power_uri,
+                DCTERMS.isPartOf,
+                MEX.cfe126af_96e5_43d7_8cbc_b52e6d3e1e3d
+            ))
+            self.graph.add((
+                power_uri,
+                MEX.e3a20fec_2cb0_44a7_8183_9d5b4d5a0886,
+                MEX.c3003dc5_e891_4d38_a5b7_f6041566ce4e
+            ))
+            self.graph.add((
+                power_uri,
+                MEX.e3a20fec_2cb0_44a7_8183_9d5b4d5a0886,
+                entity_uri
+            ))
+            self.graph.add((
+                power_uri,
+                RDFS.label,
+                Literal(
+                    f"El Poder {power_types_es[i]} de la Entidad Federal de {entity_name}", lang="es")
+            ))
+            self.graph.add((
+                power_uri,
+                RDFS.label,
+                Literal(f"The {entity_name} Federal Entity {
+                        power_types_en[i]} Power", lang="en")
+            ))
+            self.graph.add((
+                power_uri,
+                RDFS.isDefinedBy,
+                MEX["fed/ef/pow/"]
+            ))
 
 # Pipeline to generate RDF for all federal entities
 
 
-def generate_all_entities(cef_path, entity_class_uri, entities_csv):
+def generate_all_entities(cef_path, entity_class_uri, power_class_uri):
     graph = Graph()
-    entity_generator = EntityCodeGenerator(graph, entity_class_uri)
+    entity_generator = EntityCodeGenerator(graph, power_class_uri)
 
-    # Load the entities data
-    entities_df = pd.read_csv(entities_csv, encoding='latin1')
+    # Load the federal entities data
+    cef_graph = Graph()
+    cef_graph.parse(cef_path, format="turtle")
 
-    # Get unique entity codes and names
-    unique_entities = entities_df[['CVE_ENT', 'NOM_ENT']].drop_duplicates()
+    # Get all federal entities
+    for entity in cef_graph.subjects(RDF.type, MEX[entity_class_uri]):
+        labels = cef_graph.objects(entity, SKOS.prefLabel)
+        entity_name = None
+        for label in labels:
+            if label.language == "es":
+                entity_name = str(label)
+                break
 
-    for _, entity in unique_entities.iterrows():
-        entity_code = entity['CVE_ENT']
-        entity_name = entity['NOM_ENT']
-
-        # Generate RDF for the federal entity
-        entity_generator.generate_instance(
-            entity_name=entity_name,
-            entity_code=int(entity_code)
-        )
-
-        print(f"Generated code for Federal Entity {entity_name} with code {entity_code}")
+        if entity_name:
+            # Generate RDF for the federal entity
+            entity_generator.generate_instance(
+                entity_uri=entity, entity_name=entity_name)
+            print(f"Generated code for Federal Entity {entity_name}")
 
     return graph
 
 
 # Execute the process to generate RDF for all federal entities
 all_entities_graph = generate_all_entities(
-    cef_path="data/MEX/FED/OEF/CEF/main.ttl",
-    entity_class_uri="c8d8cdf0_8173_4233_9c11_a21c40337503",
-    entities_csv="/Users/danielbakas/Desktop/Catálogo áreas geoestadísticas/CATUN/AGEEML_202411211456316.csv"
+    cef_path="data/MEX/FED/EF/main.ttl",
+    entity_class_uri="fd98d032_2fdd_4491_a235_a445ec0a7bf6",
+    power_class_uri="c12531d0_53c0_40ab_8bf0_10f019459b24"
 )
 
 # Save the RDF
